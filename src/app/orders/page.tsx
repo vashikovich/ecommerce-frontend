ff064ba892c5ee419176cf5b7f02a94b42fbf22d
Indera-Aji-Waskitho <waskitho@outlook.com>
2024-06-19 09:39:23 +0700

    order detail page

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -5,6 +5,7 @@ import { GetOrdersByUserQuery } from "@/lib/queries";
 import { useQuery } from "@apollo/client";
 import dayjs from "dayjs";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function OrdersPage() {
   const getOrdersQuery = useQuery(GetOrdersByUserQuery);
@@ -16,54 +17,53 @@ export default function OrdersPage() {
   ) as Order[];
 
   return (
-    <div className="p-4 flex-col lg:max-w-screen-xl lg:mx-auto">
+    <div className="p-4 flex-col lg:max-w-screen-lg lg:mx-auto">
       <h2 className="text-3xl my-2 lg:my-10">Orders & Invoices</h2>
       <div className="w-full px-4 lg:border-2 lg:p-10">
         {orders.map((o) => (
-          <div
-            key={"id"}
-            className="flex py-3 items-center border-b-2 border-light-gray lg:py-10"
-          >
-            <div className="w-60">
-              <Image
-                src={o.items[0].imageUrl ?? ""}
-                width={150}
-                height={150}
-                alt="img"
-              />
-            </div>
-            <div className="flex flex-col lg:flex-row lg:justify-between w-full lg:gap-20 lg:h-40">
-              <div className="flex justify-between lg:hidden">
-                <h6 className="text-blue-900 font-bold">{o.status}</h6>
-                <p>{dayjs(o.date).format('D MMM YYYY')}</p>
+          <Link key={"id"} href={`/orders/${o.id}`}>
+            <div className="flex py-3 items-center border-b-2 border-light-gray lg:py-10">
+              <div className="w-60">
+                <Image
+                  src={o.items[0].imageUrl ?? ""}
+                  width={150}
+                  height={150}
+                  alt="img"
+                />
               </div>
-              <div className="flex justify-end lg:hidden">
-                <h6 className="text-medium-gray text-sm">{o.id}</h6>
-              </div>
-              <div className="lg:w-1/2">
-                <h6 className="text-xs font-bold text-light-gray">
-                  {o.items[0].brand}
-                </h6>
-                <p className="text-lg">{o.items[0].name}</p>
-                <div className="flex mt-2 text-sm">
-                  {o.items.length > 1 ? `+${o.items.length - 1} others` : ""}
-                </div>
-              </div>
-              <div className="hidden lg:flex flex-col justify-between text-right">
-                <div>
+              <div className="flex flex-col lg:flex-row lg:justify-between w-full lg:gap-20 lg:h-40">
+                <div className="flex justify-between lg:hidden">
                   <h6 className="text-blue-900 font-bold">{o.status}</h6>
-                  <p>{dayjs(o.date).format('D MMM YYYY')}</p>
+                  <p>{dayjs(o.date).format("D MMM YYYY")}</p>
+                </div>
+                <div className="flex justify-end lg:hidden">
                   <h6 className="text-medium-gray text-sm">{o.id}</h6>
                 </div>
-                <div>
+                <div className="lg:w-1/2">
+                  <h6 className="text-xs font-bold text-light-gray">
+                    {o.items[0].brand}
+                  </h6>
+                  <p className="text-lg">{o.items[0].name}</p>
+                  <div className="flex mt-2 text-sm">
+                    {o.items.length > 1 ? `+${o.items.length - 1} others` : ""}
+                  </div>
+                </div>
+                <div className="hidden lg:flex flex-col justify-between text-right">
+                  <div>
+                    <h6 className="text-blue-900 font-bold">{o.status}</h6>
+                    <p>{dayjs(o.date).format("D MMM YYYY")}</p>
+                    <h6 className="text-medium-gray text-sm">{o.id}</h6>
+                  </div>
+                  <div>
+                    <p className="font-bold text-lg">${o.totalAmount}</p>
+                  </div>
+                </div>
+                <div className="flex justify-end lg:hidden">
                   <p className="font-bold text-lg">${o.totalAmount}</p>
                 </div>
               </div>
-              <div className="flex justify-end lg:hidden">
-                <p className="font-bold text-lg">${o.totalAmount}</p>
-              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
